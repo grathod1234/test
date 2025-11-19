34b1f46dd1f869147521e709b0ed7bb28c00621d
ganesh rathod <ganeshrathod455@gmail.com>
2025-11-19 18:18:13 +0000

    adding stack.c file

---
+++ stack.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int push(){
+	printf("push opertions\n");
+}
+
+int pop(){
+	printf("pop opertion\n");
+}
+
+int isFull(){
+	print("todo\n");
+}
+
+int main(){
+	printf("this is stack\n");
+	return 0;
+}
